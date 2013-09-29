ce8b993a29f146f6635fbaa0bac35e5a668a5f2d
J. Ian Johnson <ianj@ccs.neu.edu>
2013-09-28 23:00:00 -0400

    Forgot a dependency.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 (define name "slideshow-helpers")
 (define version "1.0")
-(define deps '("base" "pict-lib" "scribble-lib" "draw-lib" "slideshow-lib"))
+(define deps '("base" "pict-lib" "scribble-lib" "draw-lib" "slideshow-lib" "unstable-lib"))
 (define setup-collects (list "slideshow-helpers"))
 (define collection "slideshow-helpers")
 (define scribblings '(("picts.scrbl")))
